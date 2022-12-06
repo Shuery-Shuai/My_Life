19d4dc7725ea7ca0d23293352679ecf5c5b54b9d
Shuery <2463253700@qq.com>
2022-12-06 18:20:58 +0800

    添加搜索功能。

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,3 +1,4 @@
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
@@ -15,4 +16,24 @@ export default defineUserConfig({
   theme,
 
   shouldPrefetch: false,
+
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          name: "category",
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          name: "tag",
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ],
 });
